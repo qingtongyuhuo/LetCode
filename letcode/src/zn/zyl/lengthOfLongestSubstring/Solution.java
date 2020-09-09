d215f662a8a67d165a0e86a54ca6bbc2a8a5acc7
asus1 <18629367430@163.com>
2020-09-09 14:00:33 +0800

    sss

---
+++ letcode/src/zn/zyl/lengthOfLongestSubstring/Solution.java
@@ -0,0 +1,36 @@
+package zn.zyl.lengthOfLongestSubstring;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static int lengthOfLongestSubstring(String s) {
+        char[] chars = s.toCharArray();
+        int length = 0;
+        Set<Character> set = new HashSet<>();
+        int j = 0;
+        for(int i = 0 ; i<chars.length ; i++){
+            char cahri = chars[i];
+            for(;j<chars.length ; j++){
+                char charj = chars[j];
+                 int temp = j-i +1;
+                if(set.contains(chars[j])){
+                    break ;
+                }
+
+                if(temp > length ){
+                    length = temp;
+                }
+                set.add(chars[j]);
+            }
+            set.remove(chars[i]);
+        }
+         return length ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcbd"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+}
